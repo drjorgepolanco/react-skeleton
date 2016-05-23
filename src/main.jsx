126c783b24a0e1229e032473cab0a0ef27158f86
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.com>
2016-05-22 20:16:37 -0400

    Create ListItem, List and ListManager, create an empty div to render the ListManager

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,7 @@ var React    = require('react');
 var ReactDOM = require('react-dom');
 
 var IngredientList = require('./components/IngredientList.jsx');
+var ListManager = require('./components/ListManager.jsx');
 
 ReactDOM.render(<IngredientList />, document.getElementById('ingredients'));
+ReactDOM.render(<ListManager />, document.getElementById('list-manager'));
